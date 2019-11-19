7e3add0031837ff073d2f5bfc0d60c5d808012dc
Richard <richard@o7n.nl>
2019-11-19 13:45:58 +0000

    Rebrand splashscreen command

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ List, import and export Kibana objects.
   If you want to retrieve all dependant objects use `-all`
 
   The output will be ndjson to stdout. If you want to have clean json instead, then use `-json`.
+
+## rebrand_splashscreen
+
+This script rebrands the Kibana splash screen. 
+
+First argument is the new title (use quotes if there are multiple words).
+
+Second argument is an SVG file with the new logo image you want to use.
+
---
+++ rebrand_splashscreen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$1" == "" ] || [ "$2" == "" ] ; then
+  echo "Usage $0 <title> <logo svg>"
+  exit
+fi
+if [ ! -f $2 ] ; then
+  echo "File $2 does not exist"
+  exit
+fi
+file -i $2 | grep " image/svg+xml;" >/dev/null
+if [ $? -ne 0 ] ; then
+  echo "$2 is not a SVG file"
+  exit
+fi
+
+TITLE=$1
+SVG=$(cat $2 | base64 -w 0)
+
+sed -i "s/PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMCIgaGVpZ2h0PSIzOSIgdmlld0JveD0iMCAwIDMwIDM5Ij4gIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+ICAgIDxwb2x5Z29uIGZpbGw9IiNGMDRFOTgiIHBvaW50cz0iMCAwIDAgMzQuNTQ3IDI5LjkyMiAuMDIiLz4gICAgPHBhdGggZmlsbD0iIzM0Mzc0MSIgZD0iTTAsMTQuNCBMMCwzNC41NDY4IEwxNC4yODcyLDE4LjA2MTIgQzEwLjA0MTYsMTUuNzM4IDUuMTgwNCwxNC40IDAsMTQuNCIvPiAgICA8cGF0aCBmaWxsPSIjMDBCRkIzIiBkPSJNMTcuMzc0MiwxOS45OTY4IEwyLjcyMSwzNi45MDQ4IEwxLjQzMzQsMzguMzg5MiBMMjkuMjYzOCwzOC4zODkyIEMyNy43NjE0LDMwLjgzODggMjMuNDA0MiwyNC4zMjY0IDE3LjM3NDIsMTkuOTk2OCIvPiAgPC9nPjwvc3ZnPg==//" /usr/share/kibana/src/legacy/ui/ui_render/views/ui_app.pug
+sed -i "s/PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMCIgaGVpZ2h0PSIzOSIgdmlld0JveD0iMCAwIDMwIDM5Ij4gIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+ICAgIDxwb2x5Z29uIGZpbGw9IiNGMDRFOTgiIHBvaW50cz0iMCAwIDAgMzQuNTQ3IDI5LjkyMiAuMDIiLz4gICAgPHBhdGggZmlsbD0iIzM0Mzc0MSIgZD0iTTAsMTQuNCBMMCwzNC41NDY4IEwxNC4yODcyLDE4LjA2MTIgQzEwLjA0MTYsMTUuNzM4IDUuMTgwNCwxNC40IDAsMTQuNCIvPiAgICA8cGF0aCBmaWxsPSIjMDBCRkIzIiBkPSJNMTcuMzc0MiwxOS45OTY4IEwyLjcyMSwzNi45MDQ4IEwxLjQzMzQsMzguMzg5MiBMMjkuMjYzOCwzOC4zODkyIEMyNy43NjE0LDMwLjgzODggMjMuNDA0MiwyNC4zMjY0IDE3LjM3NDIsMTkuOTk2OCIvPiAgPC9nPjwvc3ZnPg==/$SVG/" /usr/share/kibana/src/legacy/ui/ui_render/views/chrome.pug
+
+sed -i "s/Loading Kibana/Loading $TITLE/" /usr/share/kibana/src/legacy/ui/ui_render/views/ui_app.pug
+sed -i "s/title Kibana/title $TITLE/" /usr/share/kibana/src/legacy/ui/ui_render/views/chrome.pug
